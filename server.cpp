aca9d5c9ef3ba892d9fe9ebd9a0f31167c94badd
gyl <im.ggyyll@gmail.com>
2021-09-18 11:05:20 +0800

    add service

--- server.cpp
+++ server.cpp
@@ -1,5 +1,9 @@
+#include <memory>
 #include "connection.h"
 #include "log.h"
+
+class server_connection;
+using ConnectionPtr = std::shared_ptr<server_connection>;
 
 class server_connection : public std::enable_shared_from_this<server_connection>
 {
@@ -15,14 +19,31 @@ class server_connection : public std::enable_shared_from_this<server_connection>
         conn->startup();
     }
     void shutdown() {}
-    void on_message(const MsgPkg::codec::SharedVector& msg)
-    {
-        conn->write(msg);
-        conn->shutdown();
-    }
-    void on_close() { LOG_DEBUG << "server close"; }
+    void set_on_message_cb(const std::function<void(const ConnectionPtr&, const std::string&)>& cb) { msg_cb_ = cb; }
+    void set_on_close_cb(const std::function<void(const ConnectionPtr&)>& cb) { close_cb_ = cb; }
+    std::string address() const { return conn->address(); }
+    void write(const std::string& msg) { conn->write(msg); }
 
    private:
+    void on_message(const std::string& msg)
+    {
+        if (msg_cb_)
+        {
+            msg_cb_(shared_from_this(), msg);
+        }
+    }
+
+    void on_close()
+    {
+        if (close_cb_)
+        {
+            close_cb_(shared_from_this());
+        }
+    }
+
+   private:
+    std::function<void(const ConnectionPtr&)> close_cb_;
+    std::function<void(const ConnectionPtr&, const std::string&)> msg_cb_;
     std::shared_ptr<connection> conn;
 };
 class server
@@ -31,41 +52,114 @@ class server
     server(boost::asio::io_context& io_context, short port)
         : acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
     {
-        do_accept();
+    }
+
+   public:
+    void startup() { do_accept(); }
+    void shutdown() { do_shutdown(); }
+    void set_on_message_cb(const std::function<void(const ConnectionPtr&, const std::string&)>& cb) { msg_cb_ = cb; }
+    void set_on_close_cb(const std::function<void(const ConnectionPtr&)>& cb) { close_cb_ = cb; }
+
+   private:
+    void do_shutdown()
+    {
+        boost::system::error_code ec;
+        acceptor_.cancel(ec);
+        acceptor_.close(ec);
+    }
+    void do_accept()
+    {
+        auto fn = [this](const boost::system::error_code& ec, boost::asio::ip::tcp::socket socket)
+        {
+            if (!ec)
+            {
+                create_connection(std::move(socket));
+            }
+
+            do_accept();
+        };
+        acceptor_.async_accept(fn);
     }
 
    private:
-    void do_accept()
+    void create_connection(boost::asio::ip::tcp::socket socket)
     {
-        acceptor_.async_accept(
-            [this](std::error_code ec, boost::asio::ip::tcp::socket socket)
-            {
-                if (!ec)
-                {
-                    std::make_shared<server_connection>(std::move(socket))->startup();
-                }
-
-                do_accept();
-            });
+        auto conn = std::make_shared<server_connection>(std::move(socket));
+        conn->set_on_message_cb([this](const ConnectionPtr& conn, const std::string& msg) { on_message(conn, msg); });
+        conn->set_on_close_cb([this](const ConnectionPtr& conn) { on_close(conn); });
+        conn->startup();
+    }
+    void on_message(const ConnectionPtr& conn, const std::string& msg)
+    {
+        if (msg_cb_)
+        {
+            msg_cb_(conn, msg);
+        }
     }
 
+    void on_close(const ConnectionPtr& conn)
+    {
+        if (close_cb_)
+        {
+            close_cb_(conn);
+        }
+        else
+        {
+            LOG_DEBUG << "server close";
+        }
+    }
+
+   private:
+    std::function<void(const ConnectionPtr&)> close_cb_;
+    std::function<void(const ConnectionPtr&, const std::string&)> msg_cb_;
     boost::asio::ip::tcp::acceptor acceptor_;
+};
+
+class service
+{
+   public:
+    service() {}
+    ~service() {}
+
+   public:
+    void startup()
+    {
+        s.set_on_message_cb([this](const ConnectionPtr& conn, const std::string& msg) { on_message(conn, msg); });
+        s.set_on_close_cb([this](const ConnectionPtr& conn) { on_close(conn); });
+        s.startup();
+    }
+    void shutdown()
+    {
+        s.shutdown();
+        io_context.stop();
+    }
+
+    void run() { io_context.run(); }
+
+   private:
+    void on_message(const ConnectionPtr& conn, const std::string& msg)
+    {
+        LOG_DEBUG << conn->address() << " " << msg;
+        conn->write(msg);
+    }
+    void on_close(const ConnectionPtr& conn)
+    {
+        LOG_WAR << conn->address() << " "
+                << "close";
+    }
+
+   private:
+    boost::asio::io_context io_context;
+    server s{io_context, 3200};
 };
 
 int main(int argc, char* argv[])
 {
-    try
-    {
-        boost::asio::io_context io_context;
-
-        server s(io_context, 3200);
-
-        io_context.run();
-    }
-    catch (std::exception& e)
-    {
-        LOG_ERROR << "Exception: " << e.what();
-    }
+    LOG_INFO << "service start";
+    service s;
+    s.startup();
+    s.run();
+    LOG_INFO << "service finish";
 
     return 0;
 }
